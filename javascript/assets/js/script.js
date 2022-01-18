66e7b58a1adf03af065aa5cbf55ee03c2c9669bf
ParkJongho1 <godhojong1@gmail.com>
2022-01-18 15:58:21 +0900

    2022.01.18

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,51 +1,3 @@
-//결과보기
-//제목을 클릭했을 때 ~~~ 실행해라!
-document.querySelector(".result1t h4").addEventListener("click", function(){
-    document.querySelector(".result1").style.display = "block";
-})
-document.querySelector(".result2t h4").addEventListener("click", function(){
-    document.querySelector(".result2").style.display = "block";
-})
-document.querySelector(".result3t h4").addEventListener("click", function(){
-    document.querySelector(".result3").style.display = "block";
-})
-document.querySelector(".result4t h4").addEventListener("click", function(){
-    document.querySelector(".result4").style.display = "block";
-})
-document.querySelector(".result5t h4").addEventListener("click", function(){
-    document.querySelector(".result5").style.display = "block";
-})
-document.querySelector(".result6t h4").addEventListener("click", function(){
-    document.querySelector(".result6").style.display = "block";
-})
-document.querySelector(".result7t h4").addEventListener("click", function(){
-    document.querySelector(".result7").style.display = "block";
-})
-document.querySelector(".result8t h4").addEventListener("click", function(){
-    document.querySelector(".result8").style.display = "block";
-})
-document.querySelector(".result9t h4").addEventListener("click", function(){
-    document.querySelector(".result9").style.display = "block";
-})
-document.querySelector(".result10t h4").addEventListener("click", function(){
-    document.querySelector(".result10").style.display = "block";
-})
-document.querySelector(".result11t h4").addEventListener("click", function(){
-    document.querySelector(".result11").style.display = "block";
-})
-document.querySelector(".result12t h4").addEventListener("click", function(){
-    document.querySelector(".result12").style.display = "block";
-})
-document.querySelector(".result13t h4").addEventListener("click", function(){
-    document.querySelector(".result13").style.display = "block";
-})
-document.querySelector(".result14t h4").addEventListener("click", function(){
-    document.querySelector(".result14").style.display = "block";
-})
-document.querySelector(".result15t h4").addEventListener("click", function(){
-    document.querySelector(".result15").style.display = "block";
-})
-
 //for문을 이용해서 1~15까지 출력
 
 // for(let i=1; i<=15; i++){
@@ -63,3 +15,12 @@ document.querySelector(".result15t h4").addEventListener("click", function(){
 //     })
 // });
 
+//결과보기 버튼을 클릭하면 경고창을 띄워주세요
+// for(let i=1; i<=15; i++){
+// document.querySelector(".result"+ i +"t h4").addEventListener("click", () =>{
+//     alert("ddd");
+// });
+// }
+
+
+    
