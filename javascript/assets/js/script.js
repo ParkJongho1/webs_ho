8f52bcd52f1a06bfedad07985a31290497415fac
ParkJongho1 <godhojong1@gmail.com>
2022-01-19 10:26:44 +0900

    2022.01.19

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,26 +1,8 @@
-//for문을 이용해서 1~15까지 출력
-
-// for(let i=1; i<=15; i++){
-//     document.querySelector(".result"+i+"t h4").addEventListener("click", function(){
-//         document.querySelector(".result"+ i).style.display = "block";
-//     });
-// }
-
-
-
-//forEach문을 이용해서 1~15까지 출력
-// document.querySelectorAll(".result h4").forEach(function(el){
-//     el.addEventListener("click", function(){
-//         alert("클릭");
-//     })
-// });
-
-//결과보기 버튼을 클릭하면 경고창을 띄워주세요
-// for(let i=1; i<=15; i++){
-// document.querySelector(".result"+ i +"t h4").addEventListener("click", () =>{
-//     alert("ddd");
-// });
-// }
+document.querySelectorAll(".result").forEach(function(el){
+    el.addEventListener("click", function(){
+        el.classList.toggle("active");
+    });
+});
 
 
     
