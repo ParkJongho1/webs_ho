f0075f5749c5755c93a75f18e5d8666066249760
ParkJongho1 <godhojong1@gmail.com>
2022-02-20 23:00:35 +0900

    2022.02.20

--- javascript/effect/js/custom.js
+++ javascript/effect/js/custom.js
@@ -15,10 +15,45 @@ const modal = function () {
     })
 }
 
+const quizmodal = function () {
+    //버튼을 클릭하면 modal창을 보이게 해주세요(show 추가)
+    document.querySelector(".source-btn").addEventListener("click", () => {
+        document.querySelector("#modal").classList.add("show");
+        document.querySelector("#modal").classList.remove("hide");
+    });
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector("#modal").classList.add("hide");
+    })
+}
+
 
 //tapmenu
 const tapMenu = function () {
     const tabBtn = document.querySelectorAll(".view-title > ul > li");
+    const tabCont = document.querySelectorAll(".view-cont > div");
+
+    //
+    tabBtn.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            //모든 클래스 active 삭제
+            tabBtn.forEach(el => {
+                el.classList.remove("active");
+            });
+            //내가 클릭한건 active 추가
+            btn.classList.add("active");
+
+            //모든 컨텐츠 박스 안보이게~
+            tabCont.forEach(el => {
+                el.style.display = "none";
+            })
+            tabCont[index].style.display = "block"
+        })
+    })
+}
+
+//tapmenu
+const quizMenu = function () {
+    const tabBtn = document.querySelectorAll(".menu-bar > ul > li");
     const tabCont = document.querySelectorAll(".view-cont > div");
 
     //
