981d5875306f9cc85a30595ac1637a515e970c10
ParkJongho1 <godhojong1@gmail.com>
2022-04-08 14:16:48 +0900

    2022.04.08

--- javascript/effect/musicPlayer/api/music.js
+++ javascript/effect/musicPlayer/api/music.js
@@ -1,80 +1,62 @@
 let allMusic = [
     {
-        name : "1. Blue Day",
-        artist : "Freedom Trail Studio",
+        name : "1. 고백",
+        artist : "이석훈",
         img : "music-1",
         audio : "music-1"
     },
     {
-        name : "2. HUStlin'",
-        artist : "NEFFEX",
+        name : "2. 그날에 나는 맘이 편했을까",
+        artist : "이예준",
         img : "music-2",
         audio : "music-2"
     },
     {
-        name : "3. Sunshine",
-        artist : "Telecasted",
+        name : "3. 찰나가 영원히 될 때",
+        artist : "마크튭 (MAKTUB)",
         img : "music-3",
         audio : "music-3"
     },
     {
-        name : "4. BElieve",
-        artist : "NEFFEX",
+        name : "4. 사랑은 늘 도망가",
+        artist : "임영웅",
         img : "music-4",
         audio : "music-4"
     },
     {
-        name : "5. Thunder ",
-        artist : "Telecasted",
+        name : "5. 취중고백 ",
+        artist : "멜로망스",
         img : "music-5",
         audio : "music-5"
     },
     {
-        name : "6. THE Itch",
-        artist : "NEFFEX",
+        name : "6. 꼭 한번 만나고 싶다.",
+        artist : "이예준",
         img : "music-6",
         audio : "music-6"
     },
     {
-        name : "7. JUSt Breathing",
-        artist : "NEFFEX",
+        name : "7. 헤어질 걸 알기에",
+        artist : "허각",
         img : "music-7",
         audio : "music-7"
     },
     {
-        name : "8. DReaming On",
-        artist : "NEFFEX",
+        name : "8. StarDust love song",
+        artist : "지효 (Twice)",
         img : "music-8",
         audio : "music-8"
     },
     {
-        name : "9. Shine On",
-        artist : "Jeremy Black",
+        name : "9. 너를 생각해",
+        artist : "주시크",
         img : "music-9",
         audio : "music-9"
     },
     {
-        name : "10. Broken Ladder",
-        artist : "Jeremy Black",
+        name : "10. 미친 소리",
+        artist : "이예준",
         img : "music-10",
         audio : "music-10"
-    },
-    {
-        name : "11. StarDust love song",
-        artist : "지효 (Twice)",
-        img : "music-11",
-        audio : "music-11"
-    },
-    {
-        name : "12. 너를 생각해",
-        artist : "주시크",
-        img : "music-12",
-        audio : "music-12"
-    },
-    {
-        name : "13. 미친 소리",
-        artist : "이예준",
-        img : "music-13",
-        audio : "music-13"
     }
 ]
