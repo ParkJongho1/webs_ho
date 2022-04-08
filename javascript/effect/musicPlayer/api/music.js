0a5f0dbeac44dad720880484d82e6d1c5a9f039c
ParkJongho1 <godhojong1@gmail.com>
2022-04-08 12:27:03 +0900

    2022.04.08

--- javascript/effect/musicPlayer/api/music.js
+++ javascript/effect/musicPlayer/api/music.js
@@ -1,60 +1,60 @@
 let allMusic = [
     {
-        name : "Blue Day",
+        name : "1. Blue Day",
         artist : "Freedom Trail Studio",
         img : "music-1",
         audio : "music-1"
     },
     {
-        name : "HUStlin'",
+        name : "2. HUStlin'",
         artist : "NEFFEX",
         img : "music-2",
         audio : "music-2"
     },
     {
-        name : "Sunshine",
+        name : "3. Sunshine",
         artist : "Telecasted",
         img : "music-3",
         audio : "music-3"
     },
     {
-        name : "BElieve",
+        name : "4. BElieve",
         artist : "NEFFEX",
         img : "music-4",
         audio : "music-4"
     },
     {
-        name : "Thunder ",
+        name : "5. Thunder ",
         artist : "Telecasted",
         img : "music-5",
         audio : "music-5"
     },
     {
-        name : "THE Itch",
+        name : "6. THE Itch",
         artist : "NEFFEX",
         img : "music-6",
         audio : "music-6"
     },
     {
-        name : "JUSt Breathing",
+        name : "7. JUSt Breathing",
         artist : "NEFFEX",
         img : "music-7",
         audio : "music-7"
     },
     {
-        name : "DReaming On",
+        name : "8. DReaming On",
         artist : "NEFFEX",
         img : "music-8",
         audio : "music-8"
     },
     {
-        name : "Shine On",
+        name : "9. Shine On",
         artist : "Jeremy Black",
         img : "music-9",
         audio : "music-9"
     },
     {
-        name : "Broken Ladder",
+        name : "10. Broken Ladder",
         artist : "Jeremy Black",
         img : "music-10",
         audio : "music-10"
